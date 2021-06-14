4d4061c1eccdc2d241237d55fd027d358342d1bc
Daniil Sliusar <dsliusar@wimarksystems.com>
2021-06-14 15:10:43 +0300

    WNE-2325: Add: PostAuthVSA in Access Server struct

--- nas.go
+++ nas.go
@@ -22,8 +22,9 @@ type AccessServer struct {
 	Login    string `json:"login" bson:"login"`
 	Password string `json:"password" bson:"password"`
 
-	PreAuthVSA string `json:"pre_auth_vsa" bson:"pre_auth_vsa"`
-	CoaVSA     string `json:"coa_vsa" bson:"coa_vsa"`
+	PreAuthVSA  string `json:"pre_auth_vsa" bson:"pre_auth_vsa"`
+	PostAuthVSA string `json:"post_auth_vsa" bson:"post_auth_vsa"`
+	CoaVSA      string `json:"coa_vsa" bson:"coa_vsa"`
 
 	CoaSecret string `json:"coa_secret" bson:"coa_secret"`
 	CoaPort   int    `json:"coa_port" bson:"coa_port"`
